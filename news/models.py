b5910e760d7f013be86a0ea6f5a32dfb9dbdf2ae
aswinkumar <aswinkumarks21@gmail.com>
2018-11-11 10:16:01 +0530

    Revert "error"
    This reverts commit 10510a1d9ae4cad0a257721aa3a16e34713e4873.

--- news/models.py
+++ news/models.py
@@ -2,31 +2,19 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.contrib.auth import get_user_model
+
 # Create your models here.
-
-class Categories(models.Model):
-    category = models.CharField(max_length=30,primary_key=True)
-
-class Category_Source(models.Model):
-    category = models.ForeignKey('Categories',to_field='category',on_delete=models.CASCADE)
-    source_id = models.CharField(max_length=50)
 
 class Articles(models.Model):
     news_id = models.AutoField(primary_key=True)
+    category = models.CharField(max_length=30)
     title = models.CharField(max_length=255)
     short_description = models.CharField(max_length=500)
     url = models.CharField(max_length=200)
     urlToImage = models.CharField(max_length=200, null=True)
     author = models.CharField(max_length=50, null=True)
     publishedAt = models.DateTimeField(null = True)
-    category = models.CharField(max_length=30)
     source_id = models.CharField(max_length=100, blank=True)
-    # source_category = models.ForeignKey(Category_Source,on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title[:50]
-
-class MarkedNews(models.Model):
-    username = models.ForeignKey(get_user_model(),on_delete=models.CASCADE)
-    news_id = models.ForeignKey(Articles,to_field='news_id',on_delete=models.CASCADE)
